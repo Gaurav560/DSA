f93d2ec2958ef2833cddb8a4bc6250cf356265cd
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-19 15:25:45 +0530

    optimized the 1st Sol

--- src/Chapter1/CH4/CountDigitsInANumber.java
+++ src/Chapter1/CH4/CountDigitsInANumber.java
@@ -2,14 +2,36 @@ package Chapter1.CH4;
 
 public class CountDigitsInANumber {
 
-    //brute force Approach
+    //brute force Approach-> Time Complexity: O(n) as we have to traverse every digit
+    //u can get the same result after removing the modulo operation.
+//    public static void main(String[] args) {
+//        int number=234545;
+//        int count=0;
+//        while(number%10!=0){
+//           count +=1;
+//           number /=10;
+//        }
+//        System.out.println(count);
+//    }
+
+
+    //better approach as TC:O(1)
+//    public static void main(String[] args) {
+//        int number=234545;
+//        //this Integer.toString() will take an integer and convert it into String.
+//     String str=Integer.toString(number);
+//
+//        System.out.println(str.length());
+//    }
+
+
+
+    //best approach-> TC: O()
     public static void main(String[] args) {
-        int number=2345;
-        int count=0;
-        while(number%10!=0){
-           count +=1;
-           number /=10;
-        }
+        int number=234545;
+  int count=0;
+      count=(int) Math.log10(number)+1;
         System.out.println(count);
     }
+
 }
