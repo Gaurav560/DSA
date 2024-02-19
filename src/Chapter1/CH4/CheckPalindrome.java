6ec474992a91072da3d3dae6db65cfce89250f63
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-19 16:11:03 +0530

    PalindromeDone

---
+++ src/Chapter1/CH4/CheckPalindrome.java
@@ -0,0 +1,22 @@
+package Chapter1.CH4;
+
+public class CheckPalindrome {
+
+    public static void main(String[] args) {
+        int number=888;
+        int originalNumber = number;
+        int reverseNum=0;
+
+        while (number!=0)
+        {
+            reverseNum=reverseNum*10+number%10;
+            number /= 10;
+    }
+     //   System.out.println(reverseNum);
+        if (originalNumber==reverseNum){
+            System.out.println("Palindrome Checked: True");
+        }else{
+            System.out.println("Palindrome Checked: False");
+        }
+    }
+}
