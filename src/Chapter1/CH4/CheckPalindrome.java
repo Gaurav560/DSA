4844c2db5bc4cf3f906891bdcec272e8a275ecfb
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-19 16:13:28 +0530

    tc updated

--- src/Chapter1/CH4/CheckPalindrome.java
+++ src/Chapter1/CH4/CheckPalindrome.java
@@ -2,6 +2,7 @@ package Chapter1.CH4;
 
 public class CheckPalindrome {
 
+    //TC: O(N) ,where N is the number of digits in the numbers.
     public static void main(String[] args) {
         int number=888;
         int originalNumber = number;
