4590d152b1b1188f6f0647ce2b926ab3b9c2a2e8
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-20 09:50:17 +0530

    check for prime

---
+++ src/Chapter1/CH4/CheckForPrime.java
@@ -0,0 +1,24 @@
+package Chapter1.CH4;
+
+public class CheckForPrime {
+
+
+    //optimal force approach tc: O(Sqrt(N))
+    static boolean checkPrime(int num){
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+
+          if (num%i==0){
+              return false;
+          }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+if (checkPrime(38)){
+    System.out.println("it is a prime number.");
+}
+else{
+    System.out.println("it is not a prime number");
+}
+    }
+}
