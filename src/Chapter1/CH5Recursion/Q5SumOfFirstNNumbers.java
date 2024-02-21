091ef2451ac7185320e57786ee13c70bc6c52f21
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-21 18:04:13 +0530

    factorial and sum Of N numbers

---
+++ src/Chapter1/CH5Recursion/Q5SumOfFirstNNumbers.java
@@ -0,0 +1,29 @@
+package Chapter1.CH5Recursion;
+
+public class Q5SumOfFirstNNumbers {
+
+    //tc: O(N) as it goes a full loop.
+//    static  int sum(int n){
+//        int sum=0;
+//        for (int i = 1; i <=n ; i++) {
+//
+//            sum +=i;
+//        }
+//        return sum;
+//    }
+
+
+    //functional way
+    static  int sum(int n){
+
+        if (n==0){
+            return 0;
+        }
+
+        return  n+sum(n-1);
+
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(6));
+    }
+}
---
+++ src/Chapter1/CH5Recursion/Q6Factorial.java
@@ -0,0 +1,26 @@
+package Chapter1.CH5Recursion;
+
+public class Q6Factorial {
+
+    //iterative way
+//    static  int findFactorial(int n){
+//        int mulValue=1;
+//        for (int i = n; i >=1 ; i--) {
+//            mulValue *=i;
+//        }
+//        return  mulValue;
+//    }
+
+
+    static  int findFactorial(int n){
+     if (n==1){
+         return 1;
+     }
+     return n * findFactorial(n-1);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(findFactorial(6));
+    }
+}
