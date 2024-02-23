38baf82c557082729f66b6df3e13389632bcee45
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-23 11:34:24 +0530

    palindrome check

---
+++ src/Chapter1/CH5Recursion/PalindromeStringCheck.java
@@ -0,0 +1,48 @@
+package Chapter1.CH5Recursion;
+
+public class PalindromeStringCheck {
+
+    //tc: O(N) as . This is because each character in the
+    // input string must be copied to a new array.
+    //sc: O(N) :The space complexity is O(n) due to the
+    // allocation of a char array that is the same size as the input string.
+//using extra space
+
+//    public boolean palindromeCheck(String str){
+//        char[] ch=str.toCharArray();
+//        int j=ch.length-1;
+//        for (int i = 0; i < ch.length/2 ; i++) {
+//            if (ch[i]!=ch[j]){
+//                return false;
+//            }
+//            j--;
+//
+//        }
+//        return  true;
+//    }
+
+//without using extra space
+
+
+
+
+
+
+    //tc and sc: O(N)
+    public boolean palindromeCheck(int i, String str) {
+        if (i >= str.length()) {
+            return true;
+        }
+        if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+            return false;
+
+        }
+        return palindromeCheck(i + 1, str);
+    }
+
+    public static void main(String[] args) {
+        PalindromeStringCheck palindromeStringCheck = new PalindromeStringCheck();
+
+        System.out.println(palindromeStringCheck.palindromeCheck(0,"ABCbhDCBA"));
+    }
+}
